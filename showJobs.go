1da82e98f6090d966ff9d2fa4451698b4bee20e0
misha <mike.rubinstein@gmail.com>
2025-09-22 17:33:57 -0500

    ok, we're not showing a job from a template rather than a string - baby steps right.. got tripped up by row.scan basically silently failing wiht a mismatching argument count.. oh go, you are really something else sometimes.

--- showJobs.go
+++ showJobs.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -13,24 +14,32 @@ func ShowJob(w http.ResponseWriter, r *http.Request) {
 	parts := strings.Split(r.URL.Path, "/")
 	length := len(parts)
 	id := parts[length-1]
-	//fmt.Fprintf(w, id)
+	//fmt.Fprintf(w, id
 	query := "select * from jobs.dbo.jobs where id =" + id
 
 	row := getARow(query)
-	fmt.Fprintf(w, "<html><body>")
+	data, _ := os.ReadFile("showJob.txt")
+	jobTemplate := string(data)
+
+	//fmt.Fprintf(w, "<html><body>")
 
 	var dbid int
+	var companyID int
 	var jobTitle string
 	var jobDescription string
 	var email string
-	row.Scan(&dbid, &jobTitle, &jobDescription, &email)
+	row.Scan(&dbid, &companyID, &jobTitle, &jobDescription, &email)
+	jobTemplate = strings.ReplaceAll(jobTemplate, "$title", jobTitle)
+	jobTemplate = strings.ReplaceAll(jobTemplate, "$description", jobDescription)
+	jobTemplate = strings.ReplaceAll(jobTemplate, "$jobID", strconv.Itoa(dbid))
+	fmt.Fprintf(w, jobTemplate)
 
-	fmt.Fprintf(w, strconv.Itoa(dbid)+"<br>")
-	fmt.Fprintf(w, jobTitle+"<br>")
-	fmt.Fprintf(w, jobDescription+"<br>")
-	fmt.Fprintf(w, email)
+	//fmt.Fprintf(w, strconv.Itoa(dbid)+"<br>")
+	//fmt.Fprintf(w, jobTitle+"<br>")
+	//fmt.Fprintf(w, jobDescription+"<br>")
+	//fmt.Fprintf(w, email)
 
-	fmt.Fprintf(w, "</body></html>")
+	//fmt.Fprintf(w, "</body></html>")
 
 }
 
